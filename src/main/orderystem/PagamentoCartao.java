8ddcd960c4bc0b3d25414cb8de193ba91a5606ec
genti-eilison <fernandes.gentilalysonjac@gmail.com>
2024-11-16 14:26:29 -0300

    fix: uses calcularDesconto in processarPagamento

--- src/main/orderystem/PagamentoCartao.java
+++ src/main/orderystem/PagamentoCartao.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 public class PagamentoCartao implements Pagamento {
     @Override
     public boolean processarPagamento(Produto produto, BigDecimal valor) {
-        if ((produto.getPreco().compareTo(valor) == 0)) {
+        if ((produto.calcularDesconto().compareTo(valor) == 0)) {
             System.out.println("Pagamento em Cartão processado com sucesso!");
             return true;
         }
--- src/main/orderystem/PagamentoPix.java
+++ src/main/orderystem/PagamentoPix.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 public class PagamentoPix implements Pagamento {
     @Override
     public boolean processarPagamento(Produto produto, BigDecimal valor) {
-        if ((produto.getPreco().compareTo(valor) == 0)) {
+        if ((produto.calcularDesconto().compareTo(valor) == 0)) {
             System.out.println("Pagamento PIX processado com sucesso!");
             return true;
         }
